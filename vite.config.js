3dd437f40fbcd5c54807153f6f92cd985b008864
Jennie <dwkang0526@gmail.com>
2024-08-31 18:25:43 +0900

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+export default defineConfig({
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://backend-server:3000", // HTTPS로 설정된 백엔드 서버의 주소
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+        secure: false,
+        ws: true,
+      },
+    },
+  },
+  plugins: [react()],
+});
